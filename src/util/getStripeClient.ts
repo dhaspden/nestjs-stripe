876bb7a04f146b78142afa7f76945c77c1bd4b71
Dylan Aspden <dhaspden@gmail.com>
2019-07-04 21:04:48 -0400

    Call `setAppInfo` on Stripe client when it is created (#5)
    * feat(Stripe): call setAppInfo when creating client
    
    * feat(Stripe): use full repository url for setAppInfo

--- src/util/getStripeClient.ts
+++ src/util/getStripeClient.ts
@@ -1,7 +1,17 @@
 import * as Stripe from 'stripe';
 
+import { name, repository, version } from './../../package.json';
 import { StripeOptions } from './../interfaces';
 
 export function getStripeClient(options: StripeOptions): Stripe {
+  const stripeClient = new Stripe(options.apiKey, options.version);
+
+  // TODO: update this when @types/stripe adds `setAppInfo`
+  (stripeClient as any).setAppInfo({
+    name,
+    url: repository,
+    version,
+  });
+
   return new Stripe(options.apiKey, options.version);
 }
