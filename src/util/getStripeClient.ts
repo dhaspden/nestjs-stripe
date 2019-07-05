39097fa1e10df938c110d47887fc086cb93d7a39
Dylan Aspden <d.aspden@coretec.io>
2019-07-04 22:13:36 -0400

    fix(Stripe): require instead of import package.json

--- src/util/getStripeClient.ts
+++ src/util/getStripeClient.ts
@@ -1,17 +1,18 @@
 import * as ProxyAgent from 'https-proxy-agent';
 import * as Stripe from 'stripe';
 
-import { name, repository, version } from './../../package.json';
 import { StripeOptions } from './../interfaces';
+
+const packageJson = require('./../../package.json');
 
 export function getStripeClient(options: StripeOptions): Stripe {
   const stripeClient = new Stripe(options.apiKey, options.version);
 
   // TODO: update this when @types/stripe adds `setAppInfo`
   (stripeClient as any).setAppInfo({
-    name,
-    url: repository,
-    version,
+    name: packageJson.name,
+    url: packageJson.repository,
+    version: packageJson.version,
   });
 
   if (typeof options.httpProxy === 'string') {
