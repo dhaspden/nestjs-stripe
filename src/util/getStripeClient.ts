7073ac7e30f41c70963c9fb65f746750d4f1d351
Dylan Aspden <d.aspden@coretec.io>
2019-07-04 20:56:52 -0400

    feat(Stripe): call setAppInfo when creating client

--- src/util/getStripeClient.ts
+++ src/util/getStripeClient.ts
@@ -1,7 +1,17 @@
 import * as Stripe from 'stripe';
 
+import { author, name, version } from './../../package.json';
 import { StripeOptions } from './../interfaces';
 
 export function getStripeClient(options: StripeOptions): Stripe {
+  const stripeClient = new Stripe(options.apiKey, options.version);
+
+  // TODO: update this when @types/stripe adds `setAppInfo`
+  (stripeClient as any).setAppInfo({
+    name,
+    url: author.url,
+    version,
+  });
+
   return new Stripe(options.apiKey, options.version);
 }
