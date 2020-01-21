d94b9bb7c038c2a42dcb68caf2d7c05b48f86c7c
Keisuke Kan <9renpoto@gmail.com>
2020-01-21 17:26:16 +0900

    chore: upgrade stripe v8.x

--- src/util/getStripeClient.ts
+++ src/util/getStripeClient.ts
@@ -1,34 +1,26 @@
-import * as ProxyAgent from 'https-proxy-agent';
-import * as Stripe from 'stripe';
+import Stripe from 'stripe';
 
 import { StripeOptions } from './../interfaces';
 
 const packageJson = require('./../../package.json');
 
-export function getStripeClient(options: StripeOptions): Stripe {
-  const stripeClient = new Stripe(options.apiKey, options.version);
+export function getStripeClient({
+  apiKey,
+  apiVersion = '2019-12-03',
+  typescript = true,
+  ...options
+}: StripeOptions): Stripe {
+  const stripeClient = new Stripe(apiKey, {
+    apiVersion,
+    typescript,
+    ...options,
+  });
 
   stripeClient.setAppInfo({
     name: packageJson.name,
     url: packageJson.repository,
     version: packageJson.version,
   });
-
-  if (options.httpProxy instanceof ProxyAgent) {
-    stripeClient.setHttpAgent(options.httpProxy);
-  }
-
-  if (typeof options.maxNetworkRetries === 'number') {
-    stripeClient.setMaxNetworkRetries(options.maxNetworkRetries);
-  }
-
-  if (typeof options.requestTelemetry === 'boolean') {
-    stripeClient.setTelemetryEnabled(options.requestTelemetry);
-  }
-
-  if (typeof options.requestTimeout === 'number') {
-    stripeClient.setTimeout(options.requestTimeout);
-  }
 
   return stripeClient;
 }
