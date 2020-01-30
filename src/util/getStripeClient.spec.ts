a49ee3dbf38f6cff796a5819a290da8325ca885b
Keisuke Kan <9renpoto@gmail.com>
2020-01-30 18:27:08 +0900

    fix: remove deprecated api

--- src/util/getStripeClient.spec.ts
+++ src/util/getStripeClient.spec.ts
@@ -5,10 +5,22 @@ import { getStripeClient } from './getStripeClient';
 describe('getStripeClient', () => {
   const apiKey = 'test';
 
-  describe('when `apiKey` is provided', () => {
-    it('should return the stripe client', () => {
-      const stripeClient = getStripeClient({ apiKey });
-      expect(stripeClient).toBeInstanceOf(Stripe);
+  it('should return the stripe client', () => {
+    const stripeClient = getStripeClient({ apiKey });
+    expect(stripeClient).toBeInstanceOf(Stripe);
+  });
+
+  it('should return the stripe client with customize', () => {
+    const stripeClient = getStripeClient({
+      apiKey,
+      appInfo: {
+        name: 'name',
+        url: 'url',
+        version: 'version',
+      },
+      timeout: 10000,
+      telemetry: false,
     });
+    expect(stripeClient).toBeInstanceOf(Stripe);
   });
 });
